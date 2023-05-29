ece52fd36aea6d12d977f180fcf9ad7a3feb1462
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-05-29 09:13:56 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -5,7 +5,8 @@ class CityRepository {
       try {
          const city = await City.create({ name });
          return city;
-      }catch (error) {
+      } catch (error) {
+         console.log("Something went wrong in the repository layer");
          throw { error };
       }
   }  
@@ -15,12 +16,37 @@ class CityRepository {
            await City.destroy({
            where: {
            id: cityId
-        }
-   });
-        }catch (error) {
-           throw { error }; 
+              }
+           });
+           return true;
+       } catch (error) {
+         console.log("Something went wrong in the repository layer");
+          throw { error };                            }; 
    }
- }
+}
+  async updateCity(cityId, data) { 
+   try {
+      const city = await City.update(data, {
+         where: {
+            id: cityId
+         }
+      });
+      return city;
+   } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+  
+  async getCity(cityId){
+   try {
+      const city = await City.findByPk(cityId);
+      return City;
+   } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+   }
+  }
 }
 
 module.exports = CityRepository;
